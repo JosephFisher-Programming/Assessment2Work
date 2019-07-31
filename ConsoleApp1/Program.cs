567f5355279d9e7a861135ed5554da4660d1098d
Jon Collins <jon.collins@aie.edu.au>
2019-07-31 09:16:11 -0700

    Initial Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Raylib;
+using rl = Raylib.Raylib;
+
+static class Program
+{
+    public static void Main()
+    {
+        rl.InitWindow(640, 480, "Hello World");
+
+        while (!rl.WindowShouldClose())
+        {
+            rl.BeginDrawing();
+
+            rl.ClearBackground(Color.WHITE);
+            rl.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+            rl.EndDrawing();
+        }
+
+        rl.CloseWindow();
+    }
+}
